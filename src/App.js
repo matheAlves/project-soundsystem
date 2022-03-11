84c4eb16edaa6bab839ba3b07cd7ce57d58e3e50
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-03-10 22:37:40 -0300

    requisito 1 :heavy_check_mark:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/login';
+import Search from './pages/search';
+import Album from './pages/album';
+import Favorites from './pages/favorites';
+import Profile from './pages/profile';
+import ProfileEdit from './pages/profile-edit';
+import NotFound from './pages/not-found';
 
 class App extends React.Component {
   render() {
-    return (<p>^^</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={ Login }
+          />
+          <Route
+            path="/search"
+            component={ Search }
+          />
+          <Route
+            path="/album/:id"
+            component={ Album }
+          />
+          <Route
+            path="/favorites"
+            component={ Favorites }
+          />
+          <Route
+            path="/profile/edit"
+            component={ ProfileEdit }
+          />
+          <Route
+            path="/profile"
+            component={ Profile }
+          />
+          <Route
+            path="*"
+            component={ NotFound }
+          />
+        </Switch>
+      </BrowserRouter>
+
+    );
   }
 }
 
---
+++ src/pages/favorites.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div data-testid="page-favorites">
+        Favorites
+      </div>
+    );
+  }
+}
+export default Favorites;
