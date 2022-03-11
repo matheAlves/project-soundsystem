3d0697bf22671b78b151720d64b62366394aebfc
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-03-11 02:10:31 -0300

    requisito 2 :heavy_check_mark:

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,75 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/loading';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      buttonDisabled: true,
+      name: '',
+      loading: false,
+      redirect: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    const minLength = 3;
+    if (value.length >= minLength) {
+      this.setState({
+        buttonDisabled: false,
+        name: value,
+      });
+    } else {
+      this.setState({
+        buttonDisabled: true,
+        name: value,
+      });
+    }
+  }
+
+  handleClick = async () => {
+    const { name } = this.state;
+    this.setState({
+      loading: true,
+    });
+    await createUser({
+      name,
+    });
+    this.setState({
+      redirect: true,
+      loading: false,
+    });
+  }
+
   render() {
+    const { buttonDisabled, loading, redirect } = this.state;
+    if (loading) return <Loading />;
     return (
       <div data-testid="page-login">
-        Login
+        <form>
+          <label htmlFor="login-name-input">
+            Nome
+            <input
+              data-testid="login-name-input"
+              id="login-name-input"
+              type="text"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            disabled={ buttonDisabled }
+            data-testid="login-submit-button"
+            type="button"
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </form>
+        {redirect ? <Redirect to="/search" /> : ''}
       </div>
     );
   }
