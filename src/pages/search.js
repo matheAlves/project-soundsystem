83fb32a46493802a839a130d125c315a48a3a932
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-03-12 18:12:34 -0300

    requisito 5 :heavy_check_mark:

--- src/pages/search.js
+++ src/pages/search.js
@@ -2,11 +2,53 @@ import React, { Component } from 'react';
 import Header from '../components/header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isButtonDisabled: true,
+      search: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      search: target.value,
+    }, () => {
+      const { search } = this.state;
+      if (search.length >= 2) {
+        this.setState({
+          isButtonDisabled: false,
+        });
+      } else {
+        this.setState({
+          isButtonDisabled: true,
+        });
+      }
+    });
+  }
+
   render() {
+    const { isButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <input
+          type="search"
+          id="search"
+          name="search"
+          placeholder="Nome do artista"
+          data-testid="search-artist-input"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ isButtonDisabled }
+        >
+          Pesquisar
+        </button>
+
       </div>
     );
   }
